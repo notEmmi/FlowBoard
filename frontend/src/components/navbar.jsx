935f851d9fa975f803ac562a0f235fc1b2f2ac1e
Emrana Begum <emranabe@buffalo.edu>
2025-12-01 18:57:26 -0500

    feat: add Navbar component for navigation structure

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,41 @@
+export default function Navbar() {
+  return (
+	<div className="navbar">
+		<div className="expanded-nav">
+			<div class="nav-item">
+				
+			</div>
+
+			<div class="nav-item">
+				
+			</div>			
+			<div class="nav-item">
+				
+			</div>			
+			<div class="nav-item">
+				
+			</div>
+		</div>
+
+		<div className="minimized-nav">
+			<div class="nav-item">
+
+			</div>
+
+			<div class="nav-item">
+				
+			</div>			
+			<div class="nav-item">
+				
+			</div>			
+			<div class="nav-item">
+				
+			</div>
+
+		</div>
+
+	</div>
+
+
+  );
+}
