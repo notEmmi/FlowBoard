467a83ee8c94358cacc7fca872c04bc72f6450db
Emrana Begum <emranabe@buffalo.edu>
2025-12-01 17:23:23 -0500

    feat: add react-router-dom for routing and update App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,23 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='main'>
 
-      </div>
-      <h1>Vite + Reteracfjsrterdhft</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Routes>
+        <Route path="/" element="" />
+      </Routes>
+    </div>
   )
 }
 
