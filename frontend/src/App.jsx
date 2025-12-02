73a3624e22ef6e3e9a5f036676705495fe198f59
Emrana Begum <emranabe@buffalo.edu>
2025-12-01 20:05:17 -0500

    feat: enhance styling and structure of main components, add Navbar with icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { api } from "./lib/api";
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='main'>
-
+      <h1>FlowBodrterard</h1>
       <Routes>
-        <Route path="/" element="" />
+        <Route path="/" element={ <Home />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,16 +1,21 @@
+import { House } from 'lucide-react';
+
 export default function Navbar() {
   return (
 	<div className="navbar">
 		<div className="expanded-nav">
 			<div class="nav-item">
-				
+				<House /> <p className='nav-label'>Home</p>
 			</div>
+				<House /> <p className='nav-label'>Home</p>
 
 			<div class="nav-item">
-				
+				<House /> <p className='nav-label'>Home</p>
+
 			</div>			
 			<div class="nav-item">
-				
+				<House /> <p className='nav-label'>Home</p>
+
 			</div>			
 			<div class="nav-item">
 				
