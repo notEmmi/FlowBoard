c598fac130ebaca949d6197bad504155f74fc886
Emrana Begum <emranabe@buffalo.edu>
2026-02-02 12:52:48 -0500

    feat: create StyleGuide component for design reference

---
+++ frontend/src/pages/StyleGuide.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function StyleGuide() {
+	return (
+		<div className="style-guide">
+			<h1>Style Guide</h1>
+			<p>Welcome to the Style Guide page.</p>
+		</div>
+	);
+}
